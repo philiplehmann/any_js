23415cb34f0de1efe6a28e06b0adc36aa0893099
lukas <lukas.westermann@gmail.com>
2011-03-03 13:28:46 +0100

    release new any.js minified

--- any.mt-min.js
+++ any.mt-min.js
@@ -1,4 +1,4 @@
-// any.js 0.0.1 (rev:6d6a979)
+// any.js 0.0.1 (rev:89f5273)
 // (c) 2011 Philip Lehmann, Lukas Westermann, at-point ag
 // any.js is freely distributable under the MIT license.
 // Portions of any.js are inspired or borrowed from Underscore.js,
@@ -6,5 +6,5 @@
 // For all details and documentation:
 // TODO: gh-pages
 (function(){var b={};this._MTMozTouch=b;b.bind=function(){};b.unbind=function(){}})(this._anyNoConflict);(function(){var b={};this._MTWebkitTouch=b;b.bind=function(){};b.unbind=function(){}})(this._anyNoConflict);(function(){var b={};this._MTEmulateTouch=b;b.bind=function(){};b.unbind=function(){}})(this._anyNoConflict);
-(function(b){var g=this,a={};b.mt=a;a.VERSION="0.0.1";a.TOUCH_EVENTS=["touchstart","touchmove","touchend","gesturestart","gesturechange","gestureend"];a.support={touch:false,mozTouch:false};a.backend=null;a.bind=function(d,c,e,f){a.backend&&a.TOUCH_EVENTS.indexOf(c)!=-1?a.backend.bind(d,c,e,f):b.bind(d,c,e,f)};a.unbind=function(d,c,e,f){a.backend&&a.TOUCH_EVENTS.indexOf(c)!=-1?a.backend.unbind(d,c,e,f):b.unbind(d,c,e,f)};a.emulate=function(){a.backend=g._MTEmulateTouch};if(a.support.touch)a.backend=
-a.support.mozTouch?g._MTMozTouch:g._MTWebkitTouch})(this._anyNoConflict);
+(function(b){var g=this,a={};b.mt=a;a.VERSION="0.0.1";a.TOUCH_EVENTS=["touchstart","touchmove","touchend","gesturestart","gesturechange","gestureend"];a.support={touch:false,mozTouch:false};a.backend=null;a.bind=function(d,c,e,f){a.backend&&a.TOUCH_EVENTS.indexOf(c)!=-1?a.backend.bind(d,c,e,f):b.bind(d,c,e,f)};a.unbind=function(d,c,e,f){a.backend&&a.TOUCH_EVENTS.indexOf(c)!=-1?a.backend.unbind(d,c,e,f):b.unbind(d,c,e,f)};a.emulate=function(){a.backend=g._MTEmulateTouch};a.support.mozTouch=b._supportsEvent("moztouchstart");
+a.support.touch=b._supportsEvent("touchstart")||a.support.mozTouch;if(a.support.touch)a.backend=a.support.mozTouch?g._MTMozTouch:g._MTWebkitTouch})(this._anyNoConflict);
